355cc36c9e290c67f44fa2817c94de7bab577c0b
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-09-07 00:38:34 +0000

    imagenes

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const card = () => {
   return (
     <div class="card">
-      <img class="card-img-top" src="..." alt="Card image cap" />
+      <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap" />
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">
